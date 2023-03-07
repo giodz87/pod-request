507578093408db690ff2838bbef9992c20ee5721
GiorgiDzneladze <gio.dz@icloud.com>
2023-03-06 19:53:04 -0500

    change html css & js files

--- script.js
+++ script.js
@@ -1,10 +1,44 @@
-var btn = document.querySelector("button");
-var don = document.querySelector("strong");
+const form = document.getElementById("form");
+const email = document.getElementById("email");
+const error = document.getElementById("error");
 
-btn.addEventListener("click", () => {
-  if (don.style.display === "none") {
-    don.style.display = "none";
-  } else {
-    don.style.display = "block";
-  }
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  validateInputs();
 });
+const setError = (element, message) => {
+  const inputControl = element.parentElement;
+  const errorDisplay = inputControl.querySelector(".error");
+
+  errorDisplay.innerText = message;
+  inputControl.classList.add("error");
+  inputControl.classList.remove("success");
+};
+
+const setSuccess = (element) => {
+  const inputControl = element.parentElement;
+  const errorDisplay = inputControl.querySelector(".error");
+
+  errorDisplay.innerText = "";
+  inputControl.classList.add("success");
+  inputControl.classList.remove("error");
+};
+
+const isValidEmail = (email) => {
+  const re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+};
+
+const validateInputs = () => {
+  const emailValue = email.value.trim();
+
+  if (emailValue === "") {
+    setError(email, "Email is required");
+  } else if (!isValidEmail(emailValue)) {
+    setError(email, "Oops! Please check your email");
+  } else {
+    setSuccess(email);
+  }
+};
